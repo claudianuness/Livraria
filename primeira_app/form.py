9d8b57d8e707276f087b296d6e9ec91cb1123e88
Ênio Bazante <eniom.bazante@gmail.com>
2023-01-05 16:50:34 -0300

    Update form.py

--- primeira_app/form.py
+++ primeira_app/form.py
@@ -6,3 +6,11 @@ class LivroForm(ModelForm):
     class Meta:
         model = Livro
         fields = ["titulo", "autor", "descricao", "valor", "categoria", "isbn"]
+
+    class BuscarForm(forms.Form):
+        isbn = forms.CharField(
+            label="ISBN",
+            required=True,
+            min_length=13,
+            max_length=13,
+        )
