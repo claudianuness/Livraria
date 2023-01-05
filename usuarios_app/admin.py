db21c95b51f9beb138fb5aa6b6215b0496b9d583
Claudia Nunes <cpns@ecomp.poli.br>
2023-01-05 00:11:42 -0300

    Incluindo novos models na administração do Django

--- usuarios_app/admin.py
+++ usuarios_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Carrinho, Cliente, Livro, Autor
 
-# Register your models here.
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Carrinho)
+class CarrinhoAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    ...
