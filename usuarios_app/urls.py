ad0bcfb5d45324d7ff1ae45957de89b56b35d2ed
Wei Oliveira <wei.cesaroliveira@gmail.com>
2023-01-05 15:45:04 -0300

    resolver conflito no url

--- usuarios_app/urls.py
+++ usuarios_app/urls.py
@@ -10,7 +10,7 @@ login_view = auth_views.LoginView.as_view(
 
 urlpatterns = [
     path("login/", login_view, name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("registrar/", CriarUsuario.as_view(), name="registrar"),
+    path("logout/", auth_views.LogoutView.as_view(), name="url_logout"),
+    path("registrar/", CriarUsuario.as_view(), name="url_registrar"),
     path("carrinho/", shopping_items_add, name="carrinho"),
 ]
