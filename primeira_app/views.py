a81d6e8291d0eeb7f111e017efb9ded54408687c
Claudia <claudiap.nsilva@recife.pe.senac,br>
2023-01-06 14:32:40 -0300

    View da busca de livro

--- primeira_app/views.py
+++ primeira_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
+import requests
 from datetime import datetime
 from .models import Livro
-from .form import LivroForm
+from .form import LivroForm, BuscarForm
+
+from mysite.settings import GOOGLE_BOOKS_URL, GOOGLE_API_KEY
 
 
 def home(request):
@@ -13,19 +16,37 @@ def listagem(request):
     dados = {"livros": Livro.objects.all()}
     return render(request, "primeira_app/listagem.html", dados)
 
-def busca(request):
 
+def buscar_no_google_books(search: str) -> dict:
+    params = {
+        "key": GOOGLE_API_KEY,
+        "q": f"isbn:{search}"
+    }
 
-    if 'busca' in request.POST:
-        busca = request.POST['busca']
-        dados = {"busca": busca, "livros": Livro.objects.filter(titulo__icontains=busca)}
-        return render(request, "primeira_app/busca.html", dados)
-    else:
-        return render(request, "primeira_app/busca.html", {})
+    r = requests.get(f"{GOOGLE_BOOKS_URL}/volumes/", params=params)
+    r_json = r.json()
 
+    return r_json.get("items")
 
+def buscar(request):
+    form = BuscarForm(request.POST or None)
 
+    resultado = None
+    resultado_titulo = None
 
+    if form.is_valid():
+        print(form.cleaned_data["isbn"])
+        resultado = buscar_no_google_books(form.cleaned_data["isbn"])
+        print(resultado)
+        if resultado:
+            resultado_titulo = resultado[0]["volumeInfo"]["title"]
+
+    contexto = {
+        "form": form,
+        "resultado": resultado,
+        "resultado_titulo": resultado_titulo,
+    }
+    return render(request, "primeira_app/busca.html", contexto)
 def criar(request):
     dados = {}
     form = LivroForm(request.POST or None)
